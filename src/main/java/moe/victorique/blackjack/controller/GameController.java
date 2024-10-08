1334c7fe628fe4888779e4a72c3a2e2e37e9280b
VictoriqueMoe <loli@victorique.moe>
2024-10-08 18:10:22 +0100

    better binding

--- src/main/java/moe/victorique/blackjack/controller/GameController.java
+++ src/main/java/moe/victorique/blackjack/controller/GameController.java
@@ -5,18 +5,20 @@ import io.swagger.v3.oas.annotations.Parameter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import moe.victorique.blackjack.annotations.RequestIp;
+import moe.victorique.blackjack.annotations.DeviceId;
 import moe.victorique.blackjack.dto.ResponseMsg;
+import moe.victorique.blackjack.dto.StatusMsg;
 import moe.victorique.blackjack.entity.Game;
 import moe.victorique.blackjack.service.IUserService;
-import moe.victorique.blackjack.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -29,12 +31,13 @@ public class GameController {
 
     private final IUserService service;
 
+    @GetMapping(value = "/stats", produces = MediaType.APPLICATION_JSON_VALUE)
+    public StatusMsg getStats(final @NonNull @Parameter(hidden = true) @DeviceId String deviceId) {
+        return null;
+    }
+
     @GetMapping(value = "/history", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<ResponseMsg> getHistory(
-            final @NonNull @Parameter(hidden = true) @RequestHeader("User-Agent") String userAgent,
-            final @NonNull @Parameter(hidden = true) @RequestIp String ip
-    ) {
-        final var deviceId = getDeviceId(ip, userAgent);
+    public List<ResponseMsg> getHistory(final @NonNull @Parameter(hidden = true) @DeviceId String deviceId) {
         return this.service
                 .getAllGames(deviceId)
                 .stream()
@@ -51,10 +54,8 @@ public class GameController {
     @GetMapping(value = "/hit", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseMsg hit(
             final @NonNull @RequestParam Optional<UUID> token,
-            final @NonNull @Parameter(hidden = true) @RequestIp String ip,
-            final @NonNull @Parameter(hidden = true) @RequestHeader("User-Agent") String userAgent
+            final @NonNull @Parameter(hidden = true) @DeviceId String deviceId
     ) {
-        final var deviceId = getDeviceId(ip, userAgent);
         return this.service.getActiveGame(deviceId, token.orElse(null))
                 .map(service::hit)
                 .map(this::buildFromGame)
@@ -62,25 +63,12 @@ public class GameController {
     }
 
     @GetMapping(value = "/deal", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseMsg deal(
-            final @NonNull @Parameter(hidden = true) @RequestIp String ip,
-            final @NonNull @Parameter(hidden = true) @RequestHeader("User-Agent") String userAgent
-    ) {
-        final var deviceId = getDeviceId(ip, userAgent);
-
+    public ResponseMsg deal(final @NonNull @Parameter(hidden = true) @DeviceId String deviceId) {
         log.info("DEAL: {}", deviceId);
         return service.getActiveGame(deviceId, null)
                 .or(() -> Optional.of(this.service.newGame(deviceId)))
                 .map(this::buildFromGame)
                 .get();
-    }
-
-    private String getDeviceId(final @NonNull String ip, final @NonNull String userAgent) {
-        try {
-            return Utils.deviceHash(ip, userAgent);
-        } catch (final NoSuchAlgorithmException e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
-        }
     }
 
     private ResponseMsg buildFromGame(final @NonNull Game game) {
--- src/main/java/moe/victorique/blackjack/engine/RequestIpArgumentResolverEngine.java
+++ src/main/java/moe/victorique/blackjack/engine/RequestIpArgumentResolverEngine.java
@@ -2,19 +2,24 @@ package moe.victorique.blackjack.engine;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.NonNull;
-import moe.victorique.blackjack.annotations.RequestIp;
+import moe.victorique.blackjack.annotations.DeviceId;
+import moe.victorique.blackjack.utils.Utils;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.NoSuchAlgorithmException;
 
 public class RequestIpArgumentResolverEngine implements HandlerMethodArgumentResolver {
 
     @Override
     public boolean supportsParameter(@NonNull final MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(RequestIp.class);
+        return parameter.hasParameterAnnotation(DeviceId.class);
     }
 
     @Override
@@ -29,11 +34,24 @@ public class RequestIpArgumentResolverEngine implements HandlerMethodArgumentRes
             return null;
         }
 
+        final var ipAddress = getIpAddress(request);
+        final var ua = request.getHeader("User-Agent");
+        return getDeviceId(ipAddress, ua);
+    }
+
+    private String getIpAddress(final @NonNull HttpServletRequest request) {
         var ipAddress = request.getHeader("X-Forwarded-For");
         if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getRemoteAddr();
         }
-
         return ipAddress;
+    }
+
+    private String getDeviceId(final @NonNull String ip, final @NonNull String userAgent) {
+        try {
+            return Utils.deviceHash(ip, userAgent);
+        } catch (final NoSuchAlgorithmException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
+        }
     }
 }
